4031d3b11cc9e418b326efcc8a85d7ce0e85062e
Peter Iskandar <bassingitup@gmail.com>
2021-02-13 11:47:02 -0500

    initial commit

---
+++ src/main/kotlin/com/peteriskandar/website/WebsiteApplication.kt
@@ -0,0 +1,11 @@
+package com.peteriskandar.website
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsiteApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsiteApplication>(*args)
+}
---
+++ src/test/kotlin/com/peteriskandar/website/WebsiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.peteriskandar.website
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
